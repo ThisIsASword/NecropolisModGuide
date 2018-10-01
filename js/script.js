fbd97bc111c50392001e62cbff1ae36e8ef89f58
ThisIsASword <thisisasword@gmail.com>
2018-10-01 03:28:14 -0400

    Add images, organize, make more responsive

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Add event listeners to images that make them go all lightbox fullscreen and such
+
+// var imagePopUps = document.querySelector(".img-wrapper");
+// // imagePopUps.style.display = "none";
+// imagePopUps.addEventListener("click", function () {
+//   imagePopUps.style.position = "absolute";
+//   imagePopUps.style.width = "100%";
+//   imagePopUps.style.left = 0;
+// });
