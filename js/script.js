544155d2a5274aa16229bbdb4dbd83cc8d11b364
ThisIsASword <thisisasword@gmail.com>
2018-10-02 04:31:34 -0400

    Add image lightbox and script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 // Add event listeners to images that make them go all lightbox fullscreen and such
+var imagePopUps = document.querySelectorAll(".img-wrapper");
+var lightbox = document.querySelector("#modal-lightbox");
+var lightboxImage = document.querySelector("#lightbox-image");
 
-// var imagePopUps = document.querySelector(".img-wrapper");
-// // imagePopUps.style.display = "none";
-// imagePopUps.addEventListener("click", function () {
-//   imagePopUps.style.position = "absolute";
-//   imagePopUps.style.width = "100%";
-//   imagePopUps.style.left = 0;
-// });
+for(var i =  0; i < imagePopUps.length; i++) {
+  imagePopUps[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    // this.style.display = "none";
+    var src = this.querySelector(".screenshot").getAttribute("src");
+    lightboxImage.src = src;
+    lightbox.style.display = "block";
+  });
+}
+
+lightbox.querySelector(".close-button").addEventListener("click", function(event) {
+  event.preventDefault();
+  lightbox.style.display = "none";
+});
